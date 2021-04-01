297d90c40bfd0163276c5b88fef30ce3f7bf12b0
Wagner Gonçalves <wgnrgoncalves@gmail.com>
2021-03-31 22:25:07 -0300

    commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {ComponentsLibModule} from 'components-app001/projects/components-lib/src/public-api';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, ComponentsLibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
